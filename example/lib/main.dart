2d06cf9037030932bd3fcb056795f4f48a5de2fa
Bedirhan Saglam <bedirhansaglam270@gmail.com>
2024-05-30 19:23:25 +0300

    fix: update example sdk

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<_MyApp> {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
